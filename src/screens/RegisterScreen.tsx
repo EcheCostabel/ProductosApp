dc2b6e7ee1106f19964de1652dca76d607b04e89
EcheCostabel <exequielcostabel@gmail.com>
2023-06-01 18:00:50 -0300

    registerScreen

--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -1,10 +1,131 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import { Text, View, KeyboardAvoidingView, TextInput, Platform, TouchableOpacity, Keyboard } from 'react-native';
+import { WhiteLogo } from '../components/WhiteLogo';
+import { loginStyles } from '../theme/loginTheme';
+import { useForm } from '../hooks/useForm';
+import { StackScreenProps } from '@react-navigation/stack'
 
-export const RegisterScreen = () => {
+interface Props extends StackScreenProps<any, any>{}
+
+export const RegisterScreen = ({navigation}: Props) => {
+
+  const { email, onChange, name, password } = useForm({
+    name:'',
+    email: '',
+    password: ''
+  });
+
+  const onRegister = ( ) => {
+    console.log({email, password, name});
+    Keyboard.dismiss();
+  };
+
   return (
-    <View>
-        <Text>RegisterScreen</Text>
-    </View>
+    
+    <>
+
+      <KeyboardAvoidingView
+        style={{flex: 1, backgroundColor: '#5856D6'}}
+        behavior={( Platform.OS === 'ios') ? 'padding': 'height'}
+      >
+
+
+      <View style={loginStyles.formContainer}>
+
+        {/* Keyboard avoid view */}
+        <WhiteLogo />
+
+        <Text style={loginStyles.title}>Registro</Text>
+
+        <Text style={loginStyles.label}>Nombre:</Text>
+        <TextInput
+          placeholder='Ingrese su nombre'
+          placeholderTextColor='rgba(255,255,255,0.4)'
+          underlineColorAndroid='white'
+          style={[
+            loginStyles.inputField,
+            (Platform.OS === 'ios') && loginStyles.inputFieldIos
+          ]}
+          selectionColor='white'
+          
+          onChangeText={(value) => onChange(value.toString(), 'name')}
+          value={name}
+          onSubmitEditing={onRegister}
+
+          autoCapitalize='words'
+          autoCorrect={false}
+        />
+
+        <Text style={loginStyles.label}>Email:</Text>
+        <TextInput
+          placeholder='Ingrese su email'
+          placeholderTextColor='rgba(255,255,255,0.4)'
+          keyboardType='email-address'
+          underlineColorAndroid='white'
+          style={[
+            loginStyles.inputField,
+            (Platform.OS === 'ios') && loginStyles.inputFieldIos
+          ]}
+          selectionColor='white'
+          
+          onChangeText={(value) => onChange(value.toString(), 'email')}
+          value={email}
+          onSubmitEditing={onRegister}
+
+          autoCapitalize='none'
+          autoCorrect={false}
+        />
+
+
+
+        
+
+        <Text style={loginStyles.label}>Contraseña:</Text>
+        <TextInput
+          placeholder='********'
+          placeholderTextColor='rgba(255,255,255,0.4)'
+          underlineColorAndroid='white'
+          secureTextEntry
+          style={[
+            loginStyles.inputField,
+            (Platform.OS === 'ios') && loginStyles.inputFieldIos
+          ]}
+          selectionColor='white'
+
+          onChangeText={(value) => onChange(value.toString(), 'password')}
+          value={password}
+          onSubmitEditing={onRegister}
+
+          autoCapitalize='none'
+          autoCorrect={false}
+        />
+
+
+        {/* Boton Login */}
+
+        <View style={loginStyles.buttonContainer}>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            style={loginStyles.button}
+            onPress={onRegister}
+          > 
+            <Text style={loginStyles.buttonText}>Crear cuenta</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Crear una nueva cuenta */}
+
+        <TouchableOpacity
+          onPress={() => navigation.replace('LoginScreen')}
+          activeOpacity={0.8}
+          style={loginStyles.buttonReturn}
+        >
+          <Text style={loginStyles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        
+      </View>
+      </KeyboardAvoidingView>
+
+    </>
   )
 }
