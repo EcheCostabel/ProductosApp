e6eec95b1fa3fe3b8d7626535d92eb40f04239b6
EcheCostabel <exequielcostabel@gmail.com>
2023-06-01 11:49:12 -0300

    start project

---
+++ src/navigator/Navigator.tsx
@@ -0,0 +1,25 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { LoginScreens } from '../screens/LoginScreen';
+import { RegisterScreen } from '../screens/RegisterScreen';
+import { ProtectedScreen } from '../screens/ProtectedScreen';
+
+
+const Stack = createStackNavigator();
+
+export const Navigator = () => {
+  return (
+    <Stack.Navigator
+        screenOptions={{
+            headerShown: false,
+            cardStyle:{
+                backgroundColor: 'white'
+            }
+        }}
+    >
+      <Stack.Screen name="LoginScreen" component={LoginScreens} />
+      <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+      <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} />
+
+    </Stack.Navigator>
+  );
+}
