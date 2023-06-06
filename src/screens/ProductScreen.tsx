21c9fe2c7ed09be5f81ad305989ef1f038b7423e
EcheCostabel <exequielcostabel@gmail.com>
2023-06-06 15:52:37 -0300

    ProductScreen Design

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -1,6 +1,6 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TextInput, Button } from 'react-native';
 import { ProductsStackParams } from '../navigator/ProductsNavigator';
 import { useNavigation } from '@react-navigation/native';
 
@@ -17,11 +17,68 @@ export const ProductScreen = ({ route }: Props) => {
   navigation.setOptions({
     title: (name) ? name : 'Nuevo Producto'
   })
- }, [])
+ }, []);
 
   return (
-    <View>
-        <Text>{id} {name}</Text>
+    <View style={styles.container}>
+        <ScrollView>
+
+          <Text style={styles.label}>Nombre del producto:</Text>
+          <TextInput
+            placeholder='Producto'
+            style={styles.textInput}
+          />
+
+          {/* Picker */}
+          <Text style={styles.label}>Categoria:</Text>
+
+
+          <Button
+            title='Guardar'
+            onPress={() => {}}
+            color='#5856D6'
+          />
+
+          <View style={{flexDirection: 'row', justifyContent: 'center', marginTop: 10}}>
+
+              <Button
+                title='Camara'
+                onPress={() => {}}
+                color='#5856D6'
+              />
+              <View style={{width: 10,}} />
+
+              <Button
+                title='Galeria'
+                onPress={() => {}}
+                color='#5856D6'
+              />
+
+          </View>
+
+        </ScrollView>
     </View>
   )
-}
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 10,
+    marginHorizontal: 20
+  },
+  label: {
+    fontSize: 18
+  },
+  textInput: {
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 20,
+    borderColor: 'rgba(0,0,0,0.2)',
+    height: 45,
+    marginTop: 5,
+    marginBottom: 15
+  }
+})
